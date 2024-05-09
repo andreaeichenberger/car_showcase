ca1d85d0e5612c750cff93bc3c9b3b491bf1ed9e
andreaeichenberger <andreaeichenberger.ae@gmail.com>
2024-05-08 21:12:23 -0300

    Layout

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -4,11 +4,11 @@ import Image from 'next/image';
 
 import { CustomButtonProps } from '@/types';
 
-const CustomButton = ({ title, containerStyles, handleClick }: CustomButtonProps) => {
+const CustomButton = ({ title, containerStyles, handleClick, btnType }: CustomButtonProps) => {
   return (
     <button
     disabled={false}
-    type={'button'}
+    type={btnType || 'button'}
     className={//'custom-btn ${containerStyles}'} TO DO NO ME DEJA HACER DINÁMICO Y DEJAR EL CUSTOM BTN, entonces lo agregué en Hero.tsx
       containerStyles}
     onClick={handleClick}
